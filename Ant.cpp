0d0b482de973c89278b5f81795abce379964af29
KevinWoodsOSU <woodske@oregonstate.edu>
2017-03-09 11:30:35 -0800

    Update Ant.cpp

--- Ant.cpp
+++ Ant.cpp
@@ -1,9 +1,3 @@
-/**************************
-Kevin Woods - woodske@oregonstate.edu
-CS 162 - Fall 2016
-Assignment: Langton's ant simulation
-***************************/
-
 #include "Ant.h"
 
 Ant::Ant(int move, int** ary, int x, int y, int rmax, int cmax) {
